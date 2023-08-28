a7cbd63d9f1179c5d515a23b885e16a2367c0132
Yonahey <cherfi.ismael@gmail.comm>
2023-08-28 17:22:39 +0200

    fixing action error

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
           alt="logo"
         />
         <p>
-          Edit <code>src/App.js</code>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
       </header>
     </div>
